c7809c53366cdfbcceb70d74c8392606a5e3eb5a
Telzhanov <askhat491@gmail.com>
2017-01-21 17:21:08 +0600

    task1

---
+++ week1/task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+       bool IsPrime(int x)
+        {
+            int count = 0;
+            for (int i=1; i<=x; i++)
+            {
+                if (x % i == 0)
+                    count = count + 1;
+            }
+            if (count > 2)
+                return false;
+            else
+                return true;
+        }
+        static void Main(string[] args)
+        {
+            int a;
+            for (int i = 0; i < args.Length; i++)
+            {
+                 a = int.Parse(args[i]);
+                if (IsPrime(a) == true)
+                    Console.WriteLine(args[i]);
+            }
+            Console.ReadKey();
+           
+        }
+    }
+}
